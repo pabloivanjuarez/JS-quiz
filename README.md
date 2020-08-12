b92eb779609d766f9af8d1313491763f1c324427
pablito <pabloivanjuarez@gmail.com>
2020-08-11 18:17:00 -0700

    my second commit/still working

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS-quiz
-Here's my fun quiz!
+Here's my partially done project. I will be resubmitting it at a later date. I learned a lot from this project, and obviously have a lot more to learn. 
+
+To be continued... 
--- script.js
+++ script.js
@@ -3,15 +3,16 @@ const questiondiv = document.querySelector(".questiondiv");
 const answers = document.querySelector(".answersdiv");
 const scoreEl = document.querySelector(".scoreDisplay")
 const timerDisplay = document.querySelector(".timerDisplay")
-const hideFunc = document.querySelector(".hide");
 const endGame = document.querySelector(".record-score");
+const endBtn = document.querySelector(".recordBtn")
 var score = 0;
 var initials;
 var scoreboard = [];
 var currentQ = 0;
 var secondsLeft = 60;
-// const 
+var thisCQ;
 
+// const 
 
 //questions
 const questions = [{
@@ -25,7 +26,7 @@ const questions = [{
 }, {
     title: "3????",
     choices: ["ans1", "ans2", "ans3"],
-    answer: "3"
+    answer: "0"
 }, {
     title: "4????",
     choices: ["ans1", "ans2", "ans3"],
@@ -37,24 +38,20 @@ const questions = [{
 }];
 
 
+answers.addEventListener("click", getQ)
+questiondiv.style.display = "none";
+answers.style.display = "none";
+endGame.style.display = "none";
+
+
+
 starBtn.addEventListener("click", function () {
+    starBtn.style.display = "none";
     timer();
     getQ();
 
-    if ("clicked" === true) {
-        hide
-    }
-
-
 });
 
-
-function endQuiz() {
-    questions
-    // show user entry/score
-}
-
-// define time, seconds left
 // code for timer
 function timer() {
     var timerInterval = setInterval(function () {
@@ -64,34 +61,65 @@ function timer() {
         if (secondsLeft === 0) {
             alert("GAME OVER");
             clearInterval(timerInterval);
+            answers.style.display = "none";
+            questiondiv.style.display = "none";
 
-
+            endGame.style.display = "block";
         }
 
     }, 1000);
 }
 
-// function stopTimer() {
-//     clearInterval(timer)
 // }
 
 // questions[currentQ]
 function getQ() {
+    questiondiv.style.display = "block";
+    answers.style.display = "block";
     answers.removeChild
-    for (i = 0; i < questions.length; i++) {
-        questions.push[i];
-    }
-    // inner html, delete question/mult choice
-    var thisCQ = questions[currentQ]
-    questiondiv.innerHTML = thisCQ.title;
+
+    // }
+
+    // inner html, reveal nxt question/mult choice
+    thisCQ = questions[currentQ]
+    questiondiv.innerHTML = "";
+    questiondiv.textContent = thisCQ.title;
+    answers.innerHTML = "";
     for (i = 0; i < thisCQ.choices.length; i++) {
         var button = document.createElement("button");
         button.innerHTML = thisCQ.choices[i]
         answers.appendChild(button);
         // eventlistn recording btn clik for ans
+        button.addEventListener("click", function () {
+            // check ans func
+
+            var checkScore = event.target.textContent
+            if (checkScore == questions[currentQ].choices[questions[currentQ].answer]) {
+
+            } else {
+                console.log(555);
+                // consequence for failing to get the right ans
+            }
+            // Here I'll be calling for a function that stops outputting questions and answers, for the end of the game.
+            currentQ++
+        })
     }
-    currentQ++
-    // potentail place for endQuiz
+}
+
+
+
+// show user entry/score
+// this func doesn't work yet...>>>>
+function gameOver() {
+
+    endGame.value.trim();
+
+    if (user === "") {
+        alert("Name cannot be blank!")
+    }
+    // 
+    console.log(namezz);
+    console.log(endBtn, "dddd");
 }
 // check ans function/reveal score
 // event target/ if statement to ver if it's right or wrong
